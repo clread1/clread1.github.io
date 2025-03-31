d3c23a2abe3679dadb1b682e981024783151fb8a
clread1 <yhyungho12@gmail.com>
2025-03-31 11:40:01 +0900

    tab_list 추가

--- 프로젝트_1/js/write_page.js
+++ 프로젝트_1/js/write_page.js
@@ -1,9 +1,19 @@
-let write_article_detail = document.getElementById('write_article_detail');
-let save = document.getElementsByClassName('save_btn')[0];
-let post = document.getElementsByClassName('post_btn')[0];
-post.addEventListener('click', function(){
-    location.href = "https://clread1.github.io/프로젝트_1/html/main_page.html";
-})
+let write_article_select = document.getElementsByClassName('write_article_select')[0];
+let write_article_select_list = document.getElementsByClassName('write_article_select_list')[0];
+    write_article_select.addEventListener('click', function(){
+        write_article_select.style.color = "#b9b9b9";
+        write_article_select_list.classList.toggle('display_none');
+    })
+
+let tab_list_option = document.getElementsByClassName('tab_list_option');
+let tab_chk = false;
+    for(let i=0; i<tab_list_option.length ;i++){
+        tab_list_option[i].addEventListener('click', function(){
+            write_article_select.innerHTML = `<span class="c_black">${this.children[0].innerText}</span>`
+            tab_chk = true;
+            write_article_select_list.classList.toggle('display_none');
+        })
+    }
 
 let tag_info = document.getElementById('tag_info')
 let tag_box = document.getElementsByClassName('write_article_tag_box')[0];
@@ -68,6 +78,44 @@ let idx = 0;
         tag_input();
     })
 
-    
+let write_content_title = document.getElementsByClassName('write_content_title')[0];
+let write_article_detail = document.getElementById('write_article_detail');
+let save = document.getElementsByClassName('save_btn')[0];
+let post = document.getElementsByClassName('post_btn')[0];
+post.addEventListener('click', function(){
+    // let post_tab = write_article_select.innerText;
+    // console.log(post_tab);
+    if(tab_chk == false){
+        write_article_select.style.color = "red";
+    }
 
+    let title_chk = false;
+        if(write_content_title.value == ''){
+            write_content_title.style.color= "red";
+        }
+        else{
+            title_chk = true;
+        }
+
+    
+    let detail_chk = false;
+        if(write_article_detail.value == ''){
+            write_article_detail.style.color = "red";
+        }
+        else{
+            detail_chk = true;
+        }
+
+    if(tab_chk == true && title_chk == true && detail_chk == true){
+        alert('게시글이 등록되었습니다')
+        location.href = "https://clread1.github.io/프로젝트_1/html/main_page.html";
+    }
+})
+write_content_title.addEventListener('click', function(){
+    write_content_title.style.color= "black";
+})
+
+write_article_detail.addEventListener('click', function(){
+    write_article_detail.style.color = "black";
+})
     
